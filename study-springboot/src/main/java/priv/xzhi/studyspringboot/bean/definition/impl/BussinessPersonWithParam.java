5cee6bd0f8f63a95a8ed85c6e1f9f3e99bfa5572
chenguanzhi <chenguanzhi@gdcattsoft.com>
2019-09-03 10:01:36 +0800

    带有参数的构造方法类的装配

---
+++ study-springboot/src/main/java/priv/xzhi/studyspringboot/bean/definition/impl/BussinessPersonWithParam.java
@@ -0,0 +1,32 @@
+package priv.xzhi.studyspringboot.bean.definition.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import priv.xzhi.studyspringboot.bean.definition.Animal;
+import priv.xzhi.studyspringboot.bean.definition.Person;
+
+/**
+ * Desc:带有参数的构造方法类的装配
+ * Created by 陈冠志 on 2019-09-02 11:05.
+ */
+@Component
+public class BussinessPersonWithParam implements Person {
+
+	private Animal animal = null;
+
+	public BussinessPersonWithParam(@Autowired @Qualifier("dog") Animal animal) {
+		this.animal = animal;
+	}
+
+	@Override
+	public void service() {
+		this.animal.user();
+	}
+
+	@Override
+	public void setAnimal(Animal animal) {
+		this.animal = animal;
+	}
+}
