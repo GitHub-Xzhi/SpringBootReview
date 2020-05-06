4516334d72dae39c387ab12928ca1b67da2269b2
Xzhi <cx_zhi@qq.com>
2020-05-06 20:30:36 +0800

    1.添加：MyAuthenticationProvider类，待完善

---
+++ springboot-security/src/main/java/priv/xzhi/security/action02/config/MyAuthenticationProvider.java
@@ -0,0 +1,23 @@
+package priv.xzhi.security.action02.config;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * @author Xzhi
+ * @desc: MyAuthenticationProvider
+ * @date 2020-05-06 14:37
+ */
+public class MyAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        return null;
+    }
+}
